1d26a8f16a10aa3b8599e812110815345dc7850f
wangSir66 <915489907@qq.com>
2023-07-19 16:58:12 +0800

    微信邀请按钮屏蔽

--- src/CommonFunc.js
+++ src/CommonFunc.js
@@ -13750,7 +13750,7 @@ MJ_setWaitBtn = function (needAdjust, posAndSizeArr, posAndSizeArrX) {
             _getRoomNumBtn.visible = false;
         else
             _getRoomNumBtn.visible = IsInviteVisible() && !MjClient.remoteCfg.guestLogin;
-        _wxinviteBtn.visible = IsInviteVisible() && !MjClient.remoteCfg.guestLogin;
+        _wxinviteBtn.visible = false; //IsInviteVisible() && !MjClient.remoteCfg.guestLogin;
         _delroomBtn.visible = IsInviteVisible() || isWaitReady;
         // 山西非创建房间的人不显示解散房间按钮
         if (isJinZhongAPPType() && !IsRoomCreator()) {
@@ -13768,7 +13768,7 @@ MJ_setWaitBtn = function (needAdjust, posAndSizeArr, posAndSizeArrX) {
             _getRoomNumBtn.visible = false;
         else
             _getRoomNumBtn.visible = IsInviteVisible() && !MjClient.remoteCfg.guestLogin;
-        _wxinviteBtn.visible = IsInviteVisible() && !MjClient.remoteCfg.guestLogin;
+        _wxinviteBtn.visible = false //IsInviteVisible() && !MjClient.remoteCfg.guestLogin;
         _delroomBtn.visible = IsInviteVisible() || isWaitReady;
         // 山西非创建房间的人不显示解散房间按钮
         if (isJinZhongAPPType() && !IsRoomCreator()) {
